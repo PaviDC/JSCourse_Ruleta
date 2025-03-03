79eb19a0c55897c3442847ed45e255a571fbc28b
Pavla Luxová <pavla.luxova.cze@gmail.com>
2025-03-03 23:08:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// 1 11document.body.innerHTML = "Toto je Javascript pro úkol Ruleta";
+//document.body.innerHTML = "Toto je Javascript pro úkol Ruleta"; //zkouška stránky
 
 /*
 Na obrázku vidíte rozložení čísel na klasické Francouzské ruletě. Ruleta obsahuje čísla 0–36. Každé číslo s výjimkou nuly je buď sudé nebo liché a zároveň červené nebo černé. Pro čísla 1 až 10 a 19 až 28 platí, že lichá čísla jsou červená a sudá jsou černá. Pro zbytek platí obrácené pravidlo, tedy lichá jsou černá a sudá červená. Nula není ani lichá ani sudá, ani černá ani červená.
